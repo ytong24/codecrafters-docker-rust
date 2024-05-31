c3e060c7be0e43f30a0feabfeb044230138abb92
Yan Tong <132377526+ytong24@users.noreply.github.com>
2024-05-31 14:28:33 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,8 @@ fn main() -> Result<()> {
         let std_err = std::str::from_utf8(&output.stderr)?;
         eprint!("{}", std_err);
     } else {
-        std::process::exit(1);
+        let exit_code = output.status.code().unwrap_or(1);
+        std::process::exit(exit_code);
     }
 
     Ok(())
