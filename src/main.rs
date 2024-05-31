fb8d6e8a0966ccfff8f408ff596d670ef3368905
Yan Tong <132377526+ytong24@users.noreply.github.com>
2024-05-31 14:23:24 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use anyhow::{Context, Result};
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    eprintln!("Logs from your program will appear here!");
+    // eprintln!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage!
     let args: Vec<_> = std::env::args().collect();
@@ -18,10 +18,12 @@ fn main() -> Result<()> {
                 command, command_args
             )
         })?;
-    
+
     if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
+        print!("{}", std_out);
+        let std_err = std::str::from_utf8(&output.stderr)?;
+        eprint!("{}", std_err);
     } else {
         std::process::exit(1);
     }
