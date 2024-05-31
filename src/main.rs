f43d4ade08c13433466234e64371d99359dba89e
Yan Tong <132377526+ytong24@users.noreply.github.com>
2024-05-31 15:00:10 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use anyhow::{Context, Result};
+use std::os::unix::fs;
+use tempfile::tempdir;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
@@ -9,6 +11,24 @@ fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
+
+    // Create a tmp_dir as the root dir of the new process
+    let tmp_root = tempdir()?;
+
+    // Copy the local exec file to the tmp_dir
+    let to = tmp_root
+        .path()
+        .join(command.strip_prefix("/").unwrap_or(&command));
+    std::fs::create_dir_all(to.parent().unwrap())?;
+    std::fs::copy(command, to)?;
+
+    // Create the /dev/null under the new root
+    let dev_null = tmp_root.path().join("dev/null");
+    std::fs::create_dir_all(dev_null.parent().unwrap())?;
+    std::fs::File::create(dev_null)?;
+
+    fs::chroot(tmp_root.path())?;
+
     let output = std::process::Command::new(command)
         .args(command_args)
         .output()
@@ -19,15 +39,13 @@ fn main() -> Result<()> {
             )
         })?;
 
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        print!("{}", std_out);
-        let std_err = std::str::from_utf8(&output.stderr)?;
-        eprint!("{}", std_err);
-    } else {
-        let exit_code = output.status.code().unwrap_or(1);
-        std::process::exit(exit_code);
-    }
+    let std_out = std::str::from_utf8(&output.stdout)?;
+    print!("{}", std_out);
+    let std_err = std::str::from_utf8(&output.stderr)?;
+    eprint!("{}", std_err);
 
-    Ok(())
+    let exit_code = output.status.code().unwrap_or(1);
+    std::process::exit(exit_code);
+
+    // Ok(())
 }
